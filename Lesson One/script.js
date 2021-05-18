4604cf14be56af88679ebeb841f73c0645960a3c
Gatunye Lucky <phafixp@gmail.com>
2021-05-18 14:59:03 +0300

    Monday Class

--- Lesson One/script.js
+++ Lesson One/script.js
@@ -1,1 +1,40 @@
-document.body.innerHTML = "<h1>This is conent from javascript</h1>";
+// Values, Types, and Operators
+var age = 78;
+var name = "Joel";
+var age2 = 999.999;
+
+let num1 = 637;
+
+const PIE;
+
+// fUNCTIONS 
+function fnName(){
+
+}
+
+// Arrow function
+const fnTwo = () => {
+
+}
+
+var obj = {
+    key: "value",
+    key2: "value",
+}
+
+function students(){
+    return {
+        name: "Jack",
+        age: "some age"
+    }
+};
+
+// an object
+students();
+
+
+
+
+
+// values ... 
+// what
